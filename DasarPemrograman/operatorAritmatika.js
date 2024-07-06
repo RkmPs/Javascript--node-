eded31d622b680e8e689b45043c937e3e3129f80
RkmPs <140313382+RkmPs@users.noreply.github.com>
2024-07-06 19:32:01 +0700

    update

---
+++ DasarPemrograman/operatorAritmatika.js
@@ -0,0 +1,33 @@
+//Aritmatika
+
+let a = 5 
+let b = 2
+
+console.log(a + b)
+console.log(a - b)
+console.log(a / b)
+console.log(a * b)
+console.log(a % b)  //hasil bagi / modulus
+console.log(a ** b) //Perpangkatan
+
+console.log(a += b)
+/* output
+    7
+    5
+    2
+    10
+    1
+    25 */
+
+
+// Increment dan Decrement
+console.log("Increment decrement")
+let postfix = 5;
+console.log(postfix++);
+/* output: 5 */
+console.log(postfix);
+/* output: 6 */
+
+let prefix = 5;
+console.log(++prefix);
+/* output: 6 */
