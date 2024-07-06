d9a866215518557bf8cde2dcb4c24b8275ac363a
RkmPs <140313382+RkmPs@users.noreply.github.com>
2024-07-06 16:29:55 +0700

    add variableTipedata

---
+++ Dasar Pemrograman/variableTipedata.js
@@ -0,0 +1,29 @@
+//fundamental js
+
+let variable = 1
+const konstanta = 2
+console.log(variable+konstanta)
+
+// Tipe Data
+let undefined
+let numbers = 10 //float masuk tipedata numnber
+let BigInt =  12n // n menandakan bahwa variable bertipe bigint, operasi pemgian akan dibulatkan kebawah
+let string = "string" // bisa memakai ''
+let boolean = true
+let boolean2 = 12 > 13;
+const id1 = Symbol("id");
+
+console.log(typeof(undefined))
+console.log(typeof(numbers))
+console.log(typeof(BigInt))
+console.log(typeof(string))
+console.log(typeof(boolean))
+console.log(typeof(boolean2))
+console.log(typeof(id1))
+
+
+
+
+
+
+ 
