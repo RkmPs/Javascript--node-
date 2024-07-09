a5a63520d33064e8dc83b0e3dfbd8e5f8a477b50
RkmPs <140313382+RkmPs@users.noreply.github.com>
2024-07-09 17:32:33 +0700

    update

---
+++ DasarPemrograman/11.destructionObj.js
@@ -0,0 +1,14 @@
+/* Before ES6 
+    Destructuring dalam JavaScript merupakan sintaksis yang dapat mengeluarkan
+     nilai dari array atau properties dari sebuah object ke dalam satuan yang lebih kecil.
+    */
+
+const profile = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 18
+  }
+   
+  const firstName = profile.firstName
+  const lastName = profile.lastName
+  const age = profile.age
