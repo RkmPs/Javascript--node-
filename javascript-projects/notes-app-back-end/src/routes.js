2814f3d5d1f31b550d52decb07538b90aca622af
RkmPs <140313382+RkmPs@users.noreply.github.com>
2024-08-26 21:48:05 +0700

    update add note

---
+++ javascript-projects/notes-app-back-end/src/routes.js
@@ -0,0 +1,16 @@
+/* eslint-disable linebreak-style */
+const { addNoteHandler } = require('./handler');
+
+const routes = [
+
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: () => {
+      addNoteHandler;
+    },
+  },
+
+];
+
+module.exports = routes;
